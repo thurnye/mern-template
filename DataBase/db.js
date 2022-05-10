e82a55fbdd294a1422c15b5be7214b074e1a86d2
thurnye <danthurnye@gmail.com>
2022-05-10 01:52:07 -0400

    Update db.js

--- DataBase/db.js
+++ DataBase/db.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 // Defining the data base
 const db = mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    //useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    //useFindAndModify: false
   });
   
 module.exports = db
